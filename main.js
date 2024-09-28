9b7305133c169f8815ac59bd2d7a7badeeebbf3e
Blaviken <jarellan@cuc.edu.co>
2024-09-28 11:23:11 -0500

    Fourth commit
    Anonymous functions are changed to arrow functions

--- main.js
+++ main.js
@@ -11,27 +11,25 @@ botones.forEach(boton => {
         }
 
         if (boton.id === "borrar") {
-            if (pantalla.textContent.length === 1 || pantalla.textContent === "Error!") {
-                pantalla.textContent = "0";
-            } else {
-                pantalla.textContent = pantalla.textContent.slice(0, -1);
-            }
+            pantalla.textContent = (pantalla.textContent.length === 1 || pantalla.textContent === "Error!")
+                ? "0"
+                : pantalla.textContent.slice(0, -1);
             return;
         }
 
         if (boton.id === "igual") {
-            try {
-                pantalla.textContent = eval(pantalla.textContent);
-            } catch {
-                pantalla.textContent = "Error!";
-            }
+            pantalla.textContent = (() => {
+                try {
+                    return eval(pantalla.textContent);
+                } catch {
+                    return "Error!";
+                }
+            })();
             return;
         }
 
-        if (pantalla.textContent === "0" || pantalla.textContent === "Error!") {
-            pantalla.textContent = botonApretado;
-        } else {
-            pantalla.textContent += botonApretado;
-        }
-    })
-})
+        pantalla.textContent = (pantalla.textContent === "0" || pantalla.textContent === "Error!")
+            ? botonApretado
+            : pantalla.textContent + botonApretado;
+    });
+});
